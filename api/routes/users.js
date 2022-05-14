5bf60cf369408af076920577cd556ce6ad5975cd
DarkSoul14789 <shubhrajivshah@gmail.com>
2022-05-14 23:50:51 +0530

    Resolved bug for updating password

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CrypoJs = require("crypto-js");
+const CryptoJs = require("crypto-js");
 const verify = require("../verifyToken")
 
 // UPDATE
